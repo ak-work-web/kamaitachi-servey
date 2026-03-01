62692f0c173ad60713fa70fe1c53627dd4eb584c
ak-work-web <waku2work.it20@gmail.com>
2026-03-01 22:20:42 +0900

    選択肢が空の時はDBに登録はしない設定を実装。一覧を新しい順に実装。

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -2,7 +2,8 @@ class Topic < ApplicationRecord
   has_many :choices
 
   # 「親（Topic）と一緒に子（Choice）も保存する」
-  accepts_nested_attributes_for :choices
+  accepts_nested_attributes_for :choices,
+                                reject_if: :all_blank
 
   validates :title, presence: true
 end
