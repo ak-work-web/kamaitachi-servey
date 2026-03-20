217e165964ba80d5c5fcb39e30fcbb4ee07bb9db
ak-work-web <waku2work.it20@gmail.com>
2026-03-20 21:49:36 +0900

    投票済みの場合は別の選択肢へ上書き（Update）できる機能を追加

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,12 +3,15 @@ class VotesController < ApplicationController
     #Choiceテーブルのどの選択肢への投票か見つける
     choice = Choice.find(params[:choice_id])
 
-    # 2. その選択肢に紐づく新しい投票データを作る
+    # １. 「お題」に対して「自分」が既に投票しているか探し、なければ新しく準備する
     # ここで「誰が(current_user)」「どのお題に(choice.topic)」をセットする
-    vote = choice.votes.build(
+    vote = Vote.find_or_initialize_by(
     user: current_user,
     topic: choice.topic
     )
+
+    # 2. 選択肢を「今回選んだもの」に上書き（新規の場合もここでセットされる）
+    vote.choice = choice
 
     #３.保存処理。画面をアンケート一覧(root)へ戻す
     if vote.save
